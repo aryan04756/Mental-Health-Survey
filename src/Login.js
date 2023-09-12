1584bfba718ec746ae3caa53deb798ecce111161
aryan khamesra <aryankhamesra04756@gmail.com>
2023-09-12 23:19:38 +0530

    Newcommit

--- src/Login.js
+++ src/Login.js
@@ -3,31 +3,46 @@ import PropTypes from 'prop-types';
 import './Login.css';
 
 async function loginUser(credentials) {
- return fetch('http://localhost:5000/login', {
-   method: 'POST',
-   headers: {
-     'Content-Type': 'application/json'
-   },
-   body: JSON.stringify(credentials)
- })
-   .then(data => data.json())
+  return fetch('http://localhost:5000/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(credentials)
+  })
+    .then(data => data.json())
 }
 
 export default function Login({ setToken }) {
-  const [username, setUserName] = useState();
-  const [password, setPassword] = useState();
+  const [username, setUserName] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState(''); // Add an error state for displaying validation errors
 
   const handleSubmit = async e => {
     e.preventDefault();
+
+    // Check if either field is empty
+    if (!username || !password) {
+      setError('Username and password are required.'); // Set the error message
+      return; // Don't proceed with the login request
+    }
+
+    // Clear any previous error messages
+    setError('');
+
     const token = await loginUser({
       username,
       password
     });
-    setToken(token);
+    
+    if (token) {
+      setToken(token);
+    } else {
+      setError('Invalid username or password.'); // Set an error message for failed login
+    }
   }
-  
 
-  return(
+  return (
     <div className="login-wrapper">
       <h1>Please Log In</h1>
       <form onSubmit={handleSubmit}>
@@ -43,6 +58,7 @@ export default function Login({ setToken }) {
           <button type="submit">Submit</button>
         </div>
       </form>
+      {error && <p className="error-message">{error}</p>} {/* Display the error message if it's set */}
     </div>
   )
 }
